1710444f2997169e85406b04123a144ee897feaf
Nicolas <lonicolasfernandez@gmail.com>
2015-08-03 17:00:32 -0400

    Lets hope this works

--- signin.py
+++ signin.py
@@ -1,5 +1,11 @@
 from google.appengine.api import users
+import jinja2
+import os
 import webapp2
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class MyHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -9,8 +15,12 @@ class MyHandler(webapp2.RequestHandler):
         else:
             greeting = ('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
+        template_vars = {"name": "Nicolas"}                
+        template = jinja_environment.get_template('timeline.html')
+        self.response.out.write(template.render(template_vars))
 
-        self.response.out.write('<html><body>%s</body></html>' % greeting)
+        # self.response.out.write("<html><head><title>Timeline</title><link rel="stylesheet" type="text/css" href="style.css"></head><body><h1>Welcome to your timeline</h1><div id="JYFall">
+        #<a href="JuniorYearFall.html">Junior Year - Fall</a></div><div id="JYSpring"><a href="JuniorYearSpring.html">Junior Year - Spring</a></div id="JYSummer"><div><a href="JuniorYearSummer.html">Junior Year - Summer</a></div><div id="SYFall"><a href="SeniorYearFall.html">Senior Year - Fall</a></div id="SYSpring"><div><a href="SeniorYearSpring.html">Senior Year - Spring</a></div></body></html>")
 app = webapp2.WSGIApplication([
     ('/', MyHandler)
 ], debug=True)
